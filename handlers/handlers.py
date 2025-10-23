6baf629b70e7e8e8de2869616946de74c0f331d2
wnderbin <ghostface.host@gmail.com>
2025-10-23 20:50:24 +0900

    BIG UPDATE | change languages + sqlite.db + keyboards + update handlers

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,12 +1,13 @@
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message, FSInputFile, CallbackQuery
 from aiogram import Router, F
 from aiogram.enums import ParseMode
 
 
 from utils import api_requests
 from languages.languages import get_user_language, get_text
-from keyboards.keyboards import get_main_keyboard
+from keyboards.keyboards import get_main_keyboard, get_language_keyboard
+from database.database import db
 
 import datetime
 import json
@@ -20,21 +21,46 @@ async def cmd_start(message: Message) -> None:
     text = get_text(user_id, 'start', current_language = current_language)
     await message.answer(text, reply_markup = get_main_keyboard(user_id))
 
-@router.message(Command('help'))
-async def cmd_help(message: Message) -> None:
-    await message.reply('help....')
+@router.callback_query(F.data == 'help')
+async def callback_help(callback: CallbackQuery) -> None:
+    callback.answer()
+    user_id = callback.from_user.id
+    text = get_text(user_id, 'help')
+    await callback.message.answer(text)
 
-@router.message(Command('rate'))
-async def cmd_rate(message: Message) -> None:
+@router.callback_query(F.data == 'rate')
+async def callback_rate(callback: CallbackQuery) -> None:
+    callback.answer()
+    user_id = callback.from_user.id
     try:
         data = await api_requests.get_exchage_rate()
         filename = f'exchange_{datetime.datetime.now()}.json'
         with open(filename, 'w', encoding = 'utf-8') as f:
             json.dump(data, f, indent = 2, ensure_ascii = False)
         file = FSInputFile(filename)
-        await message.answer_document(file, caption='Текущий курс относительно USD 📊')
+        success_text = get_text(user_id, 'rate')
+        await callback.message.answer_document(file, caption=success_text)
     except Exception as e:
-        await message.answer('Ошибка во время отправки файла')
+        error_text = get_text(user_id, 'error_sending')
+        await callback.message.answer(error_text)
+
+
+@router.message(Command('language'))
+async def cmd_language(message: Message) -> None:
+    user_id = message.from_user.id
+    text = get_text(user_id, 'change_lang')
+    await message.answer(text, reply_markup=get_language_keyboard())
+
+@router.callback_query(F.data.startswith("lang_"))
+async def change_language(callback: CallbackQuery):
+    user_id = callback.from_user.id
+    language = callback.data.split("_")[1]
+    db.update_user_language(user_id, language)
+    current_language = get_user_language(user_id)
+    text = get_text(user_id, 'new_lang', current_language = current_language)
+    
+    await callback.message.edit_text(text, reply_markup=get_main_keyboard(user_id)
+    )
 
 @router.message(Command('convert'))
 async def cmd_convert(message: Message) -> None:
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -5,8 +5,8 @@ from languages.languages import get_user_language
 
 def get_language_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    builder.button('Русский 🇷🇺', callback_data = 'lang_ru')
-    builder.button('English 🇬🇧', callback_data = 'lang_en')
+    builder.button(text = 'Русский 🇷🇺', callback_data = 'lang_ru')
+    builder.button(text = 'English 🇬🇧', callback_data = 'lang_en')
     builder.adjust(2)
     return builder.as_markup()
 
@@ -15,16 +15,20 @@ def get_main_keyboard(user_id: int):
     builder = InlineKeyboardBuilder()
 
     if lang == 'en':
-        builder.button(text = "💱 Convert currency", callback_data = "convert_currency")
-        builder.button(text = "🧮 Calculator", callback_data = "calculator") 
-        builder.button(text = "🌍 Change language", callback_data = "change_language")
-        builder.button(text = "📊 My profile", callback_data = "my_profile")
+        # builder.button(text = '💱 Convert currency', callback_data = 'convert_currency')
+        # builder.button(text = '🧮 Calculator', callback_data = 'calculator')
+        builder.button(text = '📖 Help', callback_data = 'help')
+        builder.button(text = '📈 Rate', callback_data = 'rate')
+        # builder.button(text = '🌍 Change language', callback_data = 'change_language')
+        builder.button(text = '📊 My profile', callback_data = 'my_profile')
     
     else:
-        builder.button(text = "💱 Конвертировать валюту", callback_data = "convert_currency")
-        builder.button(text = "🧮 Калькулятор", callback_data = "calculator")
-        builder.button(text = "🌍 Сменить язык", callback_data = "change_language")
-        builder.button(text = "📊 Мой профиль", callback_data = "my_profile")
+        # builder.button(text = '💱 Конвертировать валюту' , callback_data = 'convert_currency')
+        #builder.button(text = '🧮 Калькулятор', callback_data = 'calculator')
+        builder.button(text = '📖 Помощь', callback_data = 'help')
+        builder.button(text = '📈 Курс', callback_data = 'rate')
+        # builder.button(text = '🌍 Сменить язык', callback_data = 'change_language')
+        builder.button(text = '📊 Мой профиль', callback_data = 'my_profile')
     
     builder.adjust(1)
     return builder.as_markup()
